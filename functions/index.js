da32ea3c31d879eb321cd582403c58b9356942cd
Mac <mac@MacBook-Pro-Mac.local>
2019-05-20 00:07:17 +0300

    second push

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,40 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.sendNotifications = functions.database.ref('/notifications/{notificationId}').onWrite((snapshot)=>{
+exports.sendNotifications = functions.database
+  .ref("/notifications/{notificationId}")
+  .onWrite(change => {
+    // https://firebase.google.com/docs/reference/functions/functions.Change.html
+
+    const snapshot = change.after;
+
+    console.log("snapshot", snapshot);
+
+    const notification1 = snapshot.val();
+
+    console.log("notification", notification1);
 
     const payload = {
-        notification: {
-            title: `New message from FLO`,
-            body: snapshot.val().message,
-            icon: '../icon.png',
-            click_action: `https://flo.health`
-        }
+      notification: {
+        title: `New message from FLO`,
+        body: JSON.stringify(notification1.message),
+        icon: "../icon.png",
+        click_action: `https://flo.health`
+      }
     };
-    
-    console.log(payload);
 
     let tokens = [];
 
-    console.log(tokens);
-    
-    return admin.database().ref('/notifications').once('value').then((snapshot) => snapshot.forEach(token => {
-
-        if (token.val()) {
-
-            tokens = tokens.concat(token.val().token);
-            return admin.messaging().sendToDevice(tokens, payload);
+    return admin.database().ref("/tokens").once("value").then(snapshot2 => {
+      snapshot2.forEach(childSnapshot => {
+        const data = childSnapshot.val();
+        if (data) {
+          tokens = tokens.concat(data.token);
         }
+      });
 
-    }))
-});
-
-
-
-
+      console.log(tokens);
+      return admin.messaging().sendToDevice(tokens, payload);
+    });
+  });
--- public/index.js
+++ public/index.js
@@ -1,67 +1,84 @@
 /** Global constants */
 const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAMMh-qyjwbI_2EjtZW3R_YgKBqele-BQ8",
-    authDomain: "home-push-d7332.firebaseapp.com",
-    databaseURL: "https://home-push-d7332.firebaseio.com",
-    projectId: "home-push-d7332",
-    storageBucket: "home-push-d7332.appspot.com",
-    messagingSenderId: "246649819096",
-    appId: "1:246649819096:web:0c525e3fbac4a11f"
+  apiKey: "AIzaSyAMMh-qyjwbI_2EjtZW3R_YgKBqele-BQ8",
+  authDomain: "home-push-d7332.firebaseapp.com",
+  databaseURL: "https://home-push-d7332.firebaseio.com",
+  projectId: "home-push-d7332",
+  storageBucket: "home-push-d7332.appspot.com",
+  messagingSenderId: "246649819096",
+  appId: "1:246649819096:web:0c525e3fbac4a11f"
 };
 
-document.addEventListener ('DOMContentLoaded', () => {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener (
-            'load', 
-            () => navigator.serviceWorker.register ('/firebase-messaging-sw.js')
+document.addEventListener("DOMContentLoaded", () => {
+  /** Form elemets and events */
+  var form = document.getElementById("send-notification-form"),
+    textAreaField = document.getElementById("notification-message");
+
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    let pushData = {
+      user: auth.currentUser.uid,
+      message: textAreaField.value,
+      icon: "icon.png"
+    };
+
+    database
+      .ref("/notifications")
+      .push(pushData)
+      .then(() => {
+        console.log(
+          "%c%s",
+          "color: green; font: 1.2rem/1 Tahoma;",
+          "Notification successful sended"
         );
-        console.log ('%c%s', 'color: green; font: 1.2rem/1 Tahoma;', "Service worker is working");
-    } else {
-        console.warn ('%c%s', 'color: red; font: 1.2rem/1 Tahoma;', "Service worker is not working");
-    }
+        textAreaField.value = "";
+      })
+      .catch(error =>
+        console.warn(
+          "%c%s",
+          "color: red; font: 1.2rem/1 Tahoma;",
+          "Notification didn't push.",
+          error
+        )
+      );
 
-    /** Form elemets and events */
-    var form = document.getElementById ('send-notification-form'),
-        textAreaField = document.getElementById ('notification-message');
-    form.addEventListener ('submit', e => {
-        e.preventDefault ();
-        let pusData = {
-            user: auth.currentUser.uid,
-            message: textAreaField.value,
-            icon: 'icon.png'
-        };
+    console.log("Request: ", pushData);
+  });
 
-        database.ref ('/notifications').push (pusData)
-        .then (() => {
-            console.log ('%c%s', 'color: green; font: 1.2rem/1 Tahoma;', "Notification successful sended")
-            textAreaField.value = '';
-        })
-        .catch(() => console.warn ('%c%s', 'color: red; font: 1.2rem/1 Tahoma;', "Notification didn't push."));
+  /** Initialisating app */
+  firebase.initializeApp(FIREBASE_CONFIG);
+  var auth = firebase.auth(),
+    messaging = firebase.messaging(),
+    database = firebase.database();
 
-        console.log ('Request: ', pusData);
-    });
+  auth
+    .signInAnonymously()
+    .then(() => {
+      return messaging.requestPermission();
+    })
+    .then(() => {
+      return messaging.getToken();
+    })
+    .then(token => {
+      database.ref("/tokens").push({
+        token: token,
+        uid: auth.currentUser.uid
+      });
+    })
+    .then(() =>
+      console.log(
+        "%c Successful access",
+        "font-weight: bold; font-size: 50px;color: green; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)"
+      )
+    )
+    .catch(error =>
+      console.warn(
+        "%c%s",
+        "color: red; font: 1.2rem/1 Tahoma;",
+        "User didn't give the permission",
+        error
+      )
+    );
 
-    /** Initialisating app */
-    firebase.initializeApp (FIREBASE_CONFIG);
-    var auth = firebase.auth (),
-        messaging = firebase.messaging (),
-        database = firebase.database ();
-
-    auth.signInAnonymously ();
-    messaging.requestPermission ()
-        .then(() => {
-            return messaging.getToken()
-        })
-        .then((token) => {
-
-            database.ref('/tokens').push({
-                token: token,
-                uid: auth.currentUser.uid
-            })
-
-        })
-        .then (() => console.log ('%c Successful access', 'font-weight: bold; font-size: 50px;color: green; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)'))
-        .catch(() => console.warn ('%c%s', 'color: red; font: 1.2rem/1 Tahoma;', "User didn't give the permission"));
-    
-    messaging.onMessage (payload => console.log ('onMessage: ', payload));
+  messaging.onMessage(payload => console.log("onMessage: ", payload));
 });
